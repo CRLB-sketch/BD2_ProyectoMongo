ff804ab07f8afeb06c9eb3b76fcbfa4410228bc3
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2023-02-13 13:33:33 -0500

    Fixing

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,104 +1,106 @@
-import React, { useState, useEffect } from "react"
-import "./Blog.css"
+import React, { useState, useEffect } from 'react'
+import './Blog.css'
 // Import Componentes
 import Post from './components/Post'
-import { Box, Typography, TextField, Button, Fab, } from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
-import NavigationIcon from '@mui/icons-material/Navigation';
+import { Box, Typography, TextField, Button, Fab } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import NavigationIcon from '@mui/icons-material/Navigation'
 
+const createPost = ({}) => ({})
 
-
-
-const createPost = ({}) => ({
-
-})
-
-
-const Blog = ({user}) => {
+const Blog = ({ user }) => {
     const [posts, setPosts] = useState(null)
-    
+    const [newPost, setNewPost] = useState({
+        user_name_id: user._id,
+        title: undefined,
+        content: undefined,
+        tags: [],
+    })
+
     useEffect(() => {
         fetch('http://localhost:8000/api/posts/')
             .then((response) => response.json())
             .then((result) => setPosts(result))
     })
-    
-    return(
-        
+
+    return (
         <div>
-            
-            
             <div id="poster">
-            <form onSubmit={createPost}>
-            <Box
-            display="flex" 
-            flexDirection={"column"} 
-            maxWidth="45%" 
-            alignItems ="center" 
-            justifyContent={"center"} 
-            margin="auto" 
-            marginTop={10}
-            padding={10}
-            borderRadius={5}
-            boxShadow={"5px 5px 10px  #ccc"}
-            sx={{
-                height: "25%",
-                backgroundColor: 'white',
-                padding: 2,
-                borderStyle: 'inset',
-                borderColor: '#344b8a',
-                
-            }}
-            >
-                <TextField
-                id="filled-multiline-flexible"
-                label="Titulo"
-                multiline
-                maxRows={2}
-                variant="filled"
-                
-            />
-                
-            <br></br>
+                <form onSubmit={createPost}>
+                    <Box
+                        display="flex"
+                        flexDirection={'column'}
+                        maxWidth="45%"
+                        alignItems="center"
+                        justifyContent={'center'}
+                        margin="auto"
+                        marginTop={10}
+                        padding={10}
+                        borderRadius={5}
+                        boxShadow={'5px 5px 10px  #ccc'}
+                        sx={{
+                            height: '25%',
+                            backgroundColor: 'white',
+                            padding: 2,
+                            borderStyle: 'inset',
+                            borderColor: '#344b8a',
+                        }}
+                    >
+                        <TextField
+                            id="filled-multiline-flexible"
+                            label="Titulo"
+                            multiline
+                            maxRows={2}
+                            variant="filled"
+                        />
 
-            <TextField
-                id="outlined-multiline-static"
-                label="Dinos que piensas.."
-                multiline
-                rows={6}
-                defaultValue=" "
-                fullWidth
-            />
-            <TextField
-                id="filled-multiline-flexible"
-                label="Añade tus tags"
-                multiline
-                maxRows={1}
-                variant="filled"
-                
-                
-            />
-            
-            <Fab color="primary" aria-label="add">
-                <AddIcon />
-            </Fab>
-            <br></br>
-            <Fab variant="extended" size="small" color="primary" aria-label="add">
-                <NavigationIcon sx={{ mr: 1 }} />
-                postear!
-            </Fab>
-            </Box>
-            </form>
+                        <br></br>
+
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Dinos que piensas.."
+                            multiline
+                            rows={6}
+                            defaultValue=" "
+                            fullWidth
+                        />
+                        <TextField
+                            id="filled-multiline-flexible"
+                            label="Añade tus tags"
+                            multiline
+                            maxRows={1}
+                            variant="filled"
+                        />
+
+                        <Fab color="primary" aria-label="add">
+                            <AddIcon
+                                onClick={() => {
+                                    alert('Agregar tag')
+                                }}
+                            />
+                        </Fab>
+                        <br></br>
+                        <Fab
+                            variant="extended"
+                            size="small"
+                            color="primary"
+                            aria-label="add"
+                        >
+                            <NavigationIcon
+                                sx={{ mr: 1 }}
+                                onClick={() => {
+                                    alert('Agregar post a la bd')
+                                }}
+                            />
+                            postear!
+                        </Fab>
+                    </Box>
+                </form>
             </div>
-            
+
             <div>
-                {posts != null && posts.map((post) => (<Post post={post}/>))}
-                
-            
-            
+                {posts != null && posts.map((post) => <Post post={post} />)}
             </div>
-
-            
         </div>
     )
 }
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,29 +1,47 @@
-import React from "react"
-import "./Header.css"
+import React from 'react'
+import './Header.css'
 
-import ButtonGroup from '@mui/material/ButtonGroup';
-import {  Avatar, Chip, Button } from "@mui/material";
+import ButtonGroup from '@mui/material/ButtonGroup'
+import { Avatar, Chip, Button } from '@mui/material'
 
+const Header = ({ user, setUser, setPage }) => {
+  return (
+    <div>
+      <header>
+        <a href="#" class="logo">
+          Twitortrix
+        </a>
 
-
-
-const Header = ({user, setPage}) => {
-    return(
-        <div>
-            
-            <header>
-                <a href="#" class="logo">Twitortrix</a>
-                
-                <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                <Chip avatar={<Avatar>M</Avatar>} label="" />
-                    <Button onClick={() => {setPage('blog');}}>Inicio</Button>
-                    <Button onClick={() => {setPage('explorar');}}>Explorar</Button>
-                    <Button onClick={() => {setPage('login');}}>Salir</Button>
-                </ButtonGroup>
-                
-            </header>
-        </div>
-    )
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Chip avatar={<Avatar>M</Avatar>} label="" />
+          <Button
+            onClick={() => {
+              setPage('blog')
+            }}
+          >
+            Inicio
+          </Button>
+          <Button
+            onClick={() => {
+              setPage('explorar')
+            }}
+          >
+            Explorar
+          </Button>
+          <Button
+            onClick={() => {
+              setUser(null)
+            }}
+          >
+            Salir
+          </Button>
+        </ButtonGroup>
+      </header>
+    </div>
+  )
 }
 
 export default Header
