afccdcaad622ab4841acce6bbb1b05c437527ac3
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2023-02-13 15:03:28 -0500

    Blog Almost Functional

--- src/pages/components/Post.js
+++ src/pages/components/Post.js
@@ -1,44 +1,45 @@
-import React from "react"
-import { Box, Typography, TextField, Button, Fab, } from "@mui/material";
+import React from 'react'
+import { Box, Typography, TextField, Button, Fab } from '@mui/material'
 
-
-const Post = ({post}) => {
-    return(
+const Post = ({ post }) => {
+    return (
         <div>
-            
-            
-            <Box 
-                display="flex" 
-                flexDirection={"column"} 
-                maxWidth={600} 
-                alignItems ="center" 
-                justifyContent={"center"} 
-                margin="auto" 
+            <Box
+                display="flex"
+                flexDirection={'column'}
+                maxWidth={600}
+                alignItems="center"
+                justifyContent={'center'}
+                margin="auto"
                 marginTop={10}
                 padding={10}
                 borderRadius={5}
-                boxShadow={"5px 5px 10px  #ccc"}
-                sx={{":hover": {boxShadow: "15px 15px 30px  #ccc"}}}
+                boxShadow={'5px 5px 10px  #ccc'}
+                sx={{ ':hover': { boxShadow: '15px 15px 30px  #ccc' } }}
             >
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-            <img src=""></img>
-            <br></br>
-            <br></br>
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+                <img src=""></img>
 
-            <TextField
-                id="outlined-multiline-static"
-                label="Comenta"
-                multiline
-                rows={1}
-                defaultValue=" "
-                fullWidth
-                padding = "2"
-            />   
-            <br></br>
+                <br></br>
+                {post.tags.length !== 0 && <h3>Tags{':'}</h3>}
+                {post.tags.map((tag) => (
+                    <p>{tag}</p>
+                ))}
+                <br></br>
 
-            <Button variant="outlined">comentarios</Button>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Comenta"
+                    multiline
+                    rows={1}
+                    defaultValue=" "
+                    fullWidth
+                    padding="2"
+                />
+                <br></br>
 
+                <Button variant="outlined">comentarios</Button>
             </Box>
         </div>
     )
