98b40c96995bc2c00ba6ba534d40658622a1cbb3
GitElean <eleanjulian@gmail.com>
2023-02-13 20:51:08 -0600

    Codigo de tableu pero no hace la visualizacion

---
+++ src/pages/Dashbord.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Visualization from './pages/components/TableauViz'
+
+const Dashbord = () => {
+    return (
+        <div>
+            <Visualization/>
+        </div>
+    );
+}
+
+export default Dashbord;
